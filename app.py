11048ef1f826f1efe810824fc340ce7ec545fd87
김명준 <kmj@gimmyeongjun-ui-MacBookPro.local>
2026-02-19 10:00:52 +0900

    auto update

--- app.py
+++ app.py
@@ -401,7 +401,7 @@ if not diag_res.empty:
                         label = f"{icon}\n{d.strftime('%m/%d')}"
                         with col:
                             st.markdown("<div class='tl-cell'>", unsafe_allow_html=True)
-                            if st.button(label, key=f"tl_{cid}_{d_str}", on_click=_set_selected_date, args=(cid, d_str)):
+                            if st.button(label, key=f"tl_{cid}_{d_str}_{idx}", on_click=_set_selected_date, args=(cid, d_str)):
                                 pass
                             st.markdown("</div>", unsafe_allow_html=True)
                 st.markdown("</div>", unsafe_allow_html=True)
